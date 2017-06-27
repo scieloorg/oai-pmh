52df28008f4dc44fe93fc7cfc78d40e9ebe1c68d
Gustavo Fonseca <gustavo@gfonseca.net>
2017-06-27 13:23:03 -0300

    Adiciona decorador ``assemblers.validate_on_debug``

--- oaipmh/assemblers.py
+++ oaipmh/assemblers.py
@@ -1,6 +1,14 @@
-import plumber
+import io
+import logging
+import functools
 
-from . import filters
+import plumber
+from lxml import etree
+
+from . import filters, catalogs
+
+
+LOGGER = logging.getLogger(__name__)
 
 
 def get_identify_pipeline():
@@ -18,6 +26,46 @@ def get_listidentifiers_pipeline():
             filters.listidentifiers, filters.tobytes)
 
 
+def get_validator():
+    xmlschema_doc = etree.parse(catalogs.SCHEMAS['OAI-PMH.xsd'])
+    return etree.XMLSchema(xmlschema_doc)
+
+
+class OAIValidator:
+    """Valida ``xml_bytes`` contra a XSD do OAI-PMH na versão 2.0.
+    """
+    def __init__(self, xml_bytes):
+        self.xml_bytes = xml_bytes
+
+    def xml_doc(self):
+        return etree.parse(io.BytesIO(self.xml_bytes))
+
+    def validate(self):
+        validator = get_validator()
+        return validator(self.xml_doc)
+
+    def __str__(self):
+        """A representação textual do validador é útil quando em conjunto
+        com a emissão de mensagens de log, pois permite que a validação seja
+        executada de maneira preguiçosa.
+        """
+        return self.validate()
+
+
+def validate_on_debug(f):
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        res = f(*args, **kwargs)
+        try:
+            LOGGER.debug('the validatation status for "%s" is: %s', res,
+                    OAIValidator(res))
+        except Exception as exc:
+            LOGGER.exception(exc)
+        return res
+    return wrapper
+
+
+@validate_on_debug
 def make_identify(request, repository):
     data = {
             'request': dict(request),
@@ -29,6 +77,7 @@ def make_identify(request, repository):
     return output
 
 
+@validate_on_debug
 def make_list_metadata_formats(request, repository, formats):
     data = {
             'request': dict(request),
@@ -41,6 +90,7 @@ def make_list_metadata_formats(request, repository, formats):
     return output
 
 
+@validate_on_debug
 def make_list_identifiers(request, repository, resources):
     data = {
             'request': dict(request),
---
+++ oaipmh/catalogs/__init__.py
@@ -0,0 +1,10 @@
+import os
+
+
+CWD = os.path.dirname(os.path.abspath(__file__))
+
+
+SCHEMAS = {
+        'OAI-PMH.xsd': os.path.join(CWD, 'OAI-PMH.xsd'),
+        }
+
